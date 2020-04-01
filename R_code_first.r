8bc5b19ed1a03c98d013b64fa226525393d9e3b7
marcocervellini <62342884+marcocervellini@users.noreply.github.com>
2020-04-01 21:58:14 +0200

    Update R_code_first.r

--- R_code_first.r
+++ R_code_first.r
@@ -3,9 +3,15 @@
 
 #we need "quotes" because we use a character element
 install.packages("sp")
+install.packages("GGally")
+install.packages("ggplot2")
 
 # we don't need "quotes" because sp is now an object inside R
 library(sp)
+library(GGally)
+library(ggplot2)
+
+library (sp) # require (sp) will also do the same job
 
 data(meuse) # there is a dataset available named meuse
 
@@ -26,18 +32,13 @@ plot(zinc,copper, col="green",pch=19)
 # cex a charcter exageration e.g. cex=2 is the double, e.g. cex=0.5 is the half
 plot(zinc,copper, col="green",pch=19,cex=2)
 
-### Multipanell in R: lesson 2020_03_27 - Moniotring Ecosystem
-
-install.packages("sp")
-install.packages("GGally")
+### lesson 2020_03_27 - Moniotring Ecosystem, Multipanell in R: 
 
 #The R package 'ggplot2' is a plotting system based on the grammar of graphics. 
 #'GGally' extends 'ggplot2' by adding several functions to reduce the complexity
 #of combining geometric objects with transformed data. Some of these functions
 #include a pairwise plot matrix, a two group pairwise plot matrix, a parallel 
 #coordinates plot, a survival plot, and several functions to plot networks.
-
-library (sp) # reqiore (sp) will also do the same job
 
 #dataset in R are called "dataframe"
 
@@ -63,7 +64,7 @@ pairs(meuse)
 
 #reducing the amount of variables , subsetting, 2 ways:
 
-# ~ is made ALT + 0126
+# ~ is made ALT + 126
 
 pairs(~ cadmium + copper + lead + zinc, data=meuse)
 
@@ -81,6 +82,57 @@ ggpairs(meuse[,3:6])
 
 # We have on the diagonal the distribution frequency, on the x axe we have the values, on the Y axe we have the frequency
 # in thi example we have an higher frequency for low value
+
+### lesson 2020_04_01 - Moniotring Ecosystem, Spatial in R: 
+
+# R code for spatial view of points
+
+# coordinates
+
+data(meuse)
+
+coordinates(meuse) = ~x+y
+
+plot(meuse)
+
+# with spplot you can visualize a plot with a precise variable and its range of values
+spplot(meuse, "zinc")
+#the same for copper with a "main" title
+spplot(meuse,"copper", main="Copper concentration")
+#another way to visualize is the function "bubble"
+bubble(meuse, "zinc")
+#another way to visualize is the function "bubble"
+bubble(meuse, "zinc", main="Zinc concentration")
+#Adding the color red to the bubbles
+bubble(meuse, "zinc", col="red", main="Zinc concentration")
+
+#Reading the file covid_agg.csv in lab_monit folder in C:/RStudio/lab_monit
+setwd("C:/RStudio/lab_monit")
+#"head=T" means that you communicate to R that there is a title in the first row(header)
+covid<-read.table("covid_agg.csv", head=T)
+covid
+
+#with function "attach" we can use directly the data to plot
+attach(covid)
+plot(country, cases)
+
+#without "attach" you have to write in this way, you have to explain to R how to red the table
+plot(covid$country,covid$cases)
+
+#without "attach" you have to write in this way, you have to explain to R how to red the table
+plot(covid$country,covid$cases)
+
+plot(country, cases, las=0) #parallel labels
+
+plot(country, cases, las=1) #orizontal labels
+
+plot(country, cases, las=2) #perpendicular labels
+
+plot(country, cases, las=3) #vertical labels
+
+plot(country, cases, las=3, cex.axis=0.5) #half of the previous size labels
+
+#to use ggplot2 you must have, data, variable and type of symbol you want to use
 
 
 
