507c460270780bcf11b713aba925dcc04e051331
marcocervellini <62342884+marcocervellini@users.noreply.github.com>
2020-04-24 16:17:14 +0200

    Update RS.r

--- RS.r
+++ RS.r
@@ -54,3 +54,23 @@ plotRGB(p224r63_2011,r=3,g=2,b=4, stretch="Lin")
 #4rows and 1column
 par(mfrow=c(4,1))
 
+dev.off
+
+# How to create a human visible image
+# Associate the Landsat band to the RGB system of the computers
+#plotRGB
+
+par(mfrow=c(1,1))# why I have to explain this to R even of I used dev.off?
+plotRGB(p224r63_2011,r=3,g=2,b=1, stretch="Lin")
+plotRGB(p224r63_2011,r=4,g=3,b=2, stretch="Lin")
+
+#exercise, put the NIr on the top of g component
+plotRGB(p224r63_2011,r=3,g=4,b=2, stretch="Lin")
+
+#exercise, put the NIr on the top of b component
+plotRGB(p224r63_2011,r=3,g=2,b=4, stretch="Lin")
+
+save.image("R_code_rs.Rdata")
+rm(list = ls())
+
+
